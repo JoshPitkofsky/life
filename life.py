8594e866f49e4c107c8c85bb9622fd048621708f
JoshPitkofsky <joshuapitkofsky@eduroam-168-79.dyn.carleton.edu>
2015-05-17 17:00:12 -0500

    fixed grid, silly me, don't use boolean logic for your graphics

--- life.py
+++ life.py
@@ -1,13 +1,26 @@
 #An extremely nieve implementation of conway's game of life
 
 #create grid to be passed in with initial state
-grid = [[|,|,|,|,|,|,|,|,|,|,|],
-		[|,+,|,|,|,|,|,|,|,|,|],
-		[|,|,+,|,|,|,|,|,|,|,|],
-		[|,|,+,|,|,|,|,|,|,|,|],
-		[|,|,+,|,|,|,|,|,|,|,|],
-		[|,|,|,|,|,|,|,|,|,|,|],
-		[|,|,|,|,|,|,|,|,|,|,|]]
+grid = [['|','|','|','|','|','|','|','|','|','|','|'],
+		['|','|','|','|','|','|','|','|','|','|','|'],
+		['|','|','+','|','|','|','|','|','|','|','|'],
+		['|','|','+','|','|','|','|','|','|','|','|'],
+		['|','|','+','|','|','|','|','|','|','|','|'],
+		['|','|','+','|','|','|','|','|','|','|','|'],
+		['|','+','|','|','|','|','|','|','|','|','|'],
+		['|','|','|','|','|','|','|','|','|','|','|'],
+		['|','|','|','|','|','|','|','|','|','|','|'],
+		['|','|','|','|','|','|','|','|','|','|','|'],
+		['|','|','|','|','|','|','|','|','|','|','|'],]
 
 #need to keep track of number of neighbors that are active at each tick
-def replace_with_neighborCount(grid)
+def replace_with_neighborCount(grid):
+	rows = len(grid) #set rows to be the number of lists in master list
+	cols = len(grid[5]) #set cols to be the number of items in a given row (this should be the same for each row, I should add check to iterate over rows and make sure they all contain the same number of elements)
+
+	NeighborCount = [] #this will soon contain the number of live neighbors that each cell has
+	for row in range (rows):
+		NeighborCount.append('[0,]')
+	print NeighborCount
+
+replace_with_neighborCount(grid)

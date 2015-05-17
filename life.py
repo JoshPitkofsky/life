16f1eb173d4503d1273d2f259ae2e24078dc34de
JoshPitkofsky <joshuapitkofsky@eduroam-168-79.dyn.carleton.edu>
2015-05-17 17:26:37 -0500

    and we are now debugging, apparently in line 34 grid is a nonetype when it shouldn't be

--- life.py
+++ life.py
@@ -25,10 +25,12 @@ def convert_to_bool(grid):
 				grid[row][col] = 0
 			else:
 				grid[row][col] = 1 
+	print grid
 
 #need to keep track of number of neighbors that are active at each tick
 def grid_with_neighborCount(grid):
 	grid = convert_to_bool(grid)
+	print grid
 	rows = len(grid) #set rows to be the number of lists in master list
 	cols = len(grid[5]) #set cols to be the number of items in a given row (this should be the same for each row, I should add check to iterate over rows and make sure they all contain the same number of elements)
 
@@ -36,10 +38,10 @@ def grid_with_neighborCount(grid):
 	NeighborCount=[[0,] * cols for row in range (rows)] #found a cool list comprehension to help make a 0 matrix (https://docs.python.org/2/tutorial/datastructures.html)
 	x=len(NeighborCount)
 	y=len(NeighborCount[5])
-
-	for row in range(rows):
-		for col in range (cols):
-			NeighborCount[x][y] = grid[]
+	#might need a buffer (rows-1) (cols-1), we'll see if i can figure out infinite or at least make it wrap around
+	for row in range(rows-1):
+		for col in range (cols-1):
+			NeighborCount[x][y] = grid[col-1][row-1]+grid[col][row-1]+grid[col+1][row-1]+grid[col-1][row]+grid[col+1][row]+grid[col+1][row+1]+grid[col][row+1]+grid[col-1][row+1]
 
 convert_to_bool(grid)
-#grid_with_neighborCount(grid)
+grid_with_neighborCount(grid)

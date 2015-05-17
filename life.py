304225122736ee5377f233509812adaaa5f5692e
JoshPitkofsky <joshuapitkofsky@eduroam-168-79.dyn.carleton.edu>
2015-05-17 17:47:13 -0500

    Gave up on idea of taking in a cool looking grid, now convert to bool is convert to cool which will make it look cool before displaying. now iterate over 8 neighbors and add up number of active neighbors per square and set that cell to that value

--- life.py
+++ life.py
@@ -1,46 +1,47 @@
 #An extremely nieve implementation of conway's game of life
 
 #create grid to be passed in with initial state
-grid = [['|','|','|','|','|','|','|','|','|','|','|'],
-		['|','|','|','|','|','|','|','|','|','|','|'],
-		['|','|','+','|','|','|','|','|','|','|','|'],
-		['|','|','+','|','|','|','|','|','|','|','|'],
-		['|','|','+','|','|','|','|','|','|','|','|'],
-		['|','|','+','|','|','|','|','|','|','|','|'],
-		['|','+','|','|','|','|','|','|','|','|','|'],
-		['|','|','|','|','|','|','|','|','|','|','|'],
-		['|','|','|','|','|','|','|','|','|','|','|'],
-		['|','|','|','|','|','|','|','|','|','|','|'],
-		['|','|','|','|','|','|','|','|','|','|','|'],]
+grid = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+		[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+		]
 
 #in order to count up neighbors lets convert grid to 1s and 0s. 
 #(I recognize this would have been easier with 1s and 0s as the grid itself but this looks cooler)
 
-def convert_to_bool(grid):
+def convert_to_cool(grid):
 	rows = len(grid)
 	cols = len(grid[5])
 	for row in range(rows):
 		for col in range(cols):
-			if grid[row][col] == "|":
-				grid[row][col] = 0
+			if grid[row][col] == 0:
+				grid[row][col] = "|"
 			else:
-				grid[row][col] = 1 
+				grid[row][col] = "+" 
 	return grid
 
 #need to keep track of number of neighbors that are active at each tick
 def grid_with_neighborCount(grid):
 	rows = len(grid) #set rows to be the number of lists in master list
 	cols = len(grid[5]) #set cols to be the number of items in a given row (this should be the same for each row, I should add check to iterate over rows and make sure they all contain the same number of elements)
-	grid = convert_to_bool(grid)
-	print grid
+	
 	NeighborCount = [] #this will soon contain the number of live neighbors that each cell has
 	NeighborCount=[[0,] * cols for row in range (rows)] #found a cool list comprehension to help make a 0 matrix (https://docs.python.org/2/tutorial/datastructures.html)
-	x=len(NeighborCount)
-	y=len(NeighborCount[5])
-	#might need a buffer (rows-1) (cols-1), we'll see if i can figure out infinite or at least make it wrap around
-	for row in range(rows-1):
-		for col in range (cols-1):
-			NeighborCount[x][y] = grid[col-1][row-1]+grid[col][row-1]+grid[col+1][row-1]+grid[col-1][row]+grid[col+1][row]+grid[col+1][row+1]+grid[col][row+1]+grid[col-1][row+1]
 
+	#might need a buffer (rows-1) (cols-1), we'll see if i can figure out infinite or at least make it wrap around
+	for row in range(1,rows-1):
+		for col in range (1,cols-1):
+			NeighborCount[row][col] = grid[row-1][col-1]+grid[row][col-1]+grid[row+1][col-1]\
+								+grid[row-1][col]					+grid[row+1][col]\
+								+grid[row+1][col+1]+grid[row][col+1]+grid[row-1][col+1]
+	return NeighborCount
 #convert_to_bool(grid)
-grid_with_neighborCount(grid)
+print grid_with_neighborCount(grid)
